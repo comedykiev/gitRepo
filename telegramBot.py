657fd413877b344511d047ccf5ba21b7ce45fa9d
comedykiev <comedykiev@gmail.com>
2017-10-04 22:40:20 +0300

    add

--- telegramBot.py
+++ telegramBot.py
@@ -1,4 +1,3 @@
-'''
 import requests
 import datetime
 url = "https://api.telegram.org/bot456772539:AAFlQa-drrGcM9xLtlqosamzOmfGUWAnZeY/"
@@ -72,5 +71,3 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         exit()
-'''
-print('hello')
